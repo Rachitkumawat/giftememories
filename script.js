8a7dcc3c069ea216c9257ec2932e0b22ec308e76
Rachitkumawat <rachitkumawat074@gmail.com>
2025-12-17 10:05:28 +0530

    FIX

--- script.js
+++ script.js
@@ -105,3 +105,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   updateCartCount();
 });
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
